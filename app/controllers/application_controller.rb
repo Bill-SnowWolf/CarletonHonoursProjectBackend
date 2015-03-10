10f049604e444275bb68d8bccebc09c74491c66f
Bill Zhou <bill@micrometrics.ca>
2015-03-09 22:41:12 -0400

    Devise Login
    * Generate User Scaffold
    * Regenerate Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Require Authorization
+  before_action :custom_authentication!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Custom Parameters during devise authentication
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:sign_up).push(:variable_name)
+  end
+
+  def custom_authentication!
+    open_routes = ['admin/','active_admin/devise/sessions']
+    authenticate_user! unless open_routes.any? { |path| 
+      "/#{params[:controller]}/".include? path
+    }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310000013) do
+ActiveRecord::Schema.define(version: 20150310022730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -27,8 +31,6 @@ ActiveRecord::Schema.define(version: 20150310000013) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
